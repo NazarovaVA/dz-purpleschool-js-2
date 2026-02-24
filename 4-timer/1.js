e97974e2c99f5ff92f043d1766597b436165ff2e
NazarovaVA <nazarovava@internet.ru>
2026-02-24 10:52:49 +0300

    6.Таймеры

--- 4-timer/1.js
+++ 4-timer/1.js
@@ -4,8 +4,13 @@ function timer(){
     const newYear = Number(new Date('01-01-2027'));
     const interval = setInterval(() => {
         const nowTime = Number(new Date());
-        const res = new Date(newYear - nowTime);
-        document.querySelector('.h2').innerText = `${res.getMonth()} месяцев, ${res.getDate()} дней, ${res.getHours()} часов, ${res.getMinutes()} минут, ${res.getSeconds()} секунд`;
+        const res = newYear - nowTime;
+        const months = Math.trunc(res / 2592000000);
+        const days = Math.trunc((res % 2592000000) / 86400000);
+        const hours = Math.trunc((res % 2592000000 % 86400000) / 3600000);
+        const minutes = Math.trunc((res % 2592000000 % 86400000 % 3600000) / 60000);
+        const seconds = Math.trunc((res % 2592000000 % 86400000 % 3600000 % 60000) / 1000);
+        document.querySelector('.h2').innerText = `${months} месяцев, ${days} дней, ${hours} часов, ${minutes} минут, ${seconds} секунд`;
     },1000);
 }
 
